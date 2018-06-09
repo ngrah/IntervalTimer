dfeda9c8cf4bf4db00bb71c98a64416ac2cd81c3
ngrah <grahnick@gmail.com>
2018-06-09 12:51:40 -0400

    Initial Commit

---
+++ IntervalTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IntervalTimer
+//
+//  Created by Nick Grah on 6/9/18.
+//  Copyright © 2018 InTimer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
